54c76cc4e683dade035b7f18b3dbdf17da9ddc58
kanishkaisuru <sampathisuru516@gmail.com>
2020-06-03 17:47:07 +0530

    test

--- game.js
+++ game.js
@@ -85,7 +85,7 @@ window.onload = function () {
         physics: {
             default: 'arcade',
             arcade: {
-                debug: true,
+                debug: false,
                 gravity: {
                     y: 0 //the game gravity
                 }
